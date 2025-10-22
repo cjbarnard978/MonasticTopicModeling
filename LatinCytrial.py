f0e173d3ff63dbe8101c139ce4e416368ff70245
Cecilia Barnard <barnar3@clemson.edu>
2025-10-22 12:53:10 -0400

    defined custom entities and model

--- LatinCytrial.py
+++ LatinCytrial.py
@@ -15,3 +15,13 @@ import sys
 from pathlib import Path 
 
 
+
+def load_spacy_model(model_name="la_core_web_sm")
+    try: 
+        nlp = spacy.load("la_core_web_sm")
+        print("model loaded")
+    except OSError:
+        print("latin model not found")
+        sys.exit(1)
+    
+
