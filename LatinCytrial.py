815765a38441d0f9ab1bac7d6cb3d11e2cacc4ba
Cecilia Barnard <barnar3@clemson.edu>
2025-10-22 11:47:04 -0400

    started requirements.txt file to try and install the compatible package of SpaCy

---
+++ LatinCytrial.py
@@ -0,0 +1,16 @@
+#Hibernia 
+#Hiberniae
+#Hiberniam
+#Hiberniarum 
+#Hiberniis
+#Hibernias
+
+#python3 -m venv TM_env
+#source TM_env/bin/activate
+
+import spacy
+import pandas as pd
+import argparse
+import sys
+from pathlib import Path
+
